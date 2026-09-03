c7381ee09648d166532dc9c9d2e362ec0f96bef5
Finn Somerville <fsomerville10@gmail.com>
2026-09-03 14:18:39 +0100

    Rebuild plant cards with photo-led design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(text)
   end
+
+  def watering_status(plant)
+    return "Never watered" if plant.last_watered_on.nil?
+
+    days = (Date.today - plant.last_watered_on).to_i
+
+    case days
+    when 0 then "Watered today"
+    when 1 then "Watered yesterday"
+    else "Watered #{days} days ago"
+    end
+  end
+
+  def watering_overdue?(plant)
+    plant.last_watered_on.nil? || (Date.today - plant.last_watered_on).to_i > 7
+  end
 end
