9241a7d650ede83ad4533ad21a7eaa41cb65def6
Noah Klemmer Domonkos <klemmernoah@icloud.com>
2026-09-03 13:10:00 +0100

    Add watering shortcuts and plant image uploads

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(email: "message-controller@example.com", password: "password")
+    @plant = @user.plants.create!(nickname: "Fern", species: "Boston fern")
+    @chat = @user.chats.create!(plant: @plant, title: "Untitled")
+    sign_in @user
+  end
+
+  test "chat form accepts an optional image" do
+    get chat_url(@chat)
+
+    assert_response :success
+    assert_select "input[type=file][name='message[image]'][accept*='image/jpeg']", count: 1
+    assert_select "textarea[name='message[content]'][required]", count: 0
+  end
+
+  test "sends an uploaded image to the AI and saves it with the message" do
+    fake_chat = fake_ai_chat
+    upload = fixture_file_upload(Rails.root.join("app/assets/images/logo.png"), "image/png")
+
+    with_fake_ai_chat(fake_chat) do
+      post chat_messages_url(@chat), params: { message: { content: "", image: upload } }
+    end
+
+    assert_redirected_to chat_path(@chat)
+    user_message = @chat.messages.find_by!(role: "user")
+    assert_predicate user_message.image, :attached?
+    assert_equal "Please diagnose the problem visible in this plant photo.", fake_chat.prompt
+    assert fake_chat.received_existing_image
+    assert_equal "Plant photo diagnosis", @chat.reload.title
+  end
+
+  private
+
+  def with_fake_ai_chat(fake_chat)
+    original_chat_method = RubyLLM.method(:chat)
+    RubyLLM.define_singleton_method(:chat) { fake_chat }
+    yield
+  ensure
+    RubyLLM.define_singleton_method(:chat, original_chat_method)
+  end
+
+  def fake_ai_chat
+    Struct.new(:prompt, :received_existing_image) do
+      def add_message(*) = self
+      def with_instructions(*) = self
+
+      def ask(prompt, with: {})
+        self.prompt = prompt
+        self.received_existing_image = File.exist?(with[:image])
+        Struct.new(:content).new("The plant needs help.")
+      end
+    end.new
+  end
 end
--- test/controllers/plants_controller_test.rb
+++ test/controllers/plants_controller_test.rb
@@ -1,23 +1,93 @@
 require "test_helper"
 
 class PlantsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get plants_index_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(email: "plant-owner@example.com", password: "password")
+    @plant = @user.plants.create!(nickname: "Fern", species: "Boston fern", last_watered_on: Date.new(2026, 8, 30))
+    sign_in @user
   end
 
-  test "should get show" do
-    get plants_show_url
+  test "show displays a watered today button" do
+    get plant_url(@plant)
+
     assert_response :success
+    assert_select ".row .col-md-6" do
+      assert_select "form[action=?]", watered_today_plant_path(@plant) do
+        assert_select "button", text: /Watered today/
+      end
+    end
   end
 
-  test "should get new" do
-    get plants_new_url
+  test "index displays a watered today button for each plant" do
+    get plants_url
+
     assert_response :success
+    assert_select "form[action=?]", watered_today_plant_path(@plant) do
+      assert_select "button", text: /Watered today/
+    end
   end
 
-  test "should get edit" do
-    get plants_edit_url
+  test "edit does not display the watered today button" do
+    get edit_plant_url(@plant)
+
     assert_response :success
+    assert_select "form[action=?]", watered_today_plant_path(@plant), count: 0
+  end
+
+  test "edit displays one calendar date field instead of separate date selects" do
+    get edit_plant_url(@plant)
+
+    assert_response :success
+    assert_select "input[type=date][name='plant[last_watered_on]']", count: 1
+    assert_select "select[name^='plant[last_watered_on']", count: 0
+  end
+
+  test "new plant form accepts a plant photo" do
+    get new_plant_url
+
+    assert_response :success
+    assert_select "input[type=file][name='plant[photo]'][accept*='image/jpeg']", count: 1
+  end
+
+  test "plant photo is displayed on the index and show pages" do
+    @plant.photo.attach(
+      io: File.open(Rails.root.join("app/assets/images/logo.png")),
+      filename: "fern.png",
+      content_type: "image/png"
+    )
+
+    get plants_url
+    assert_select "img[alt='Photo of Fern']", count: 1
+
+    get plant_url(@plant)
+    assert_select "img[alt='Photo of Fern']", count: 1
+  end
+
+  test "watered today sets the last watered date to today" do
+    travel_to Time.zone.local(2026, 9, 3, 10) do
+      patch watered_today_plant_url(@plant)
+
+      assert_redirected_to plant_path(@plant)
+      assert_equal Date.new(2026, 9, 3), @plant.reload.last_watered_on
+      assert_equal "Fern was watered today.", flash[:notice]
+    end
+  end
+
+  test "watered today returns to the page where it was clicked" do
+    patch watered_today_plant_url(@plant), headers: { "HTTP_REFERER" => plants_url }
+
+    assert_redirected_to plants_path
+  end
+
+  test "watered today cannot update another user's plant" do
+    other_user = User.create!(email: "other-owner@example.com", password: "password")
+    other_plant = other_user.plants.create!(nickname: "Palm", species: "Parlor palm")
+
+    patch watered_today_plant_url(other_plant)
+
+    assert_response :not_found
+    assert_nil other_plant.reload.last_watered_on
   end
 end
